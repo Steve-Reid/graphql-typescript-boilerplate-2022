da328a98ea3775fa5ecd21e5b57f2543d8ec999c
SteveReid <steve@appby.design>
2022-10-02 20:38:03 +0100

    set up apollo server and test query

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-var': 0,
     'prefer-destructuring': 0,
     'prettier/prettier': 'error',
+    'import/prefer-default-export': 0,
     'react/jsx-props-no-spreading': 0,
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
